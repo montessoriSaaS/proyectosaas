02049a907b4f96baec1f8d58e1104fd3b75d3cfc
unknown <montessorisaas@gmail.com>
2024-05-13 18:10:52 -0600

    update 6

--- public/registroEscuelas.js
+++ public/registroEscuelas.js
@@ -3,23 +3,24 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.1/firebase
 import {
   getAuth,
   createUserWithEmailAndPassword,
+  deleteUser
 } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-auth.js";
 import {
   getFirestore,
   doc,
   setDoc,
-  deleteUser,
+  deleteDoc,
+  collection,
+  addDoc,
+  onSnapshot,
+  getDocs
 } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-firestore.js";
 import {
   getFunctions,
-  httpsCallable,
+  httpsCallable
 } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-functions.js";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAEy6y6RQvOsZWW1OHQMxwT7dLZvIzMV3I",
   authDomain: "deskmontessori-a3cb2.firebaseapp.com",
@@ -29,16 +30,14 @@ const firebaseConfig = {
   appId: "1:325693574028:web:272bfa54294c335d6d64a9",
   measurementId: "G-RLJVEHLMY5",
 };
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const functions = getFunctions(app);
-const payments = getStripePayments(app, {
-  productsCollection: "products",
-  customersCollection: "customers",
-});
 
+// Handle subscription creation and payment
 document
   .getElementById("payButton")
   .addEventListener("click", async function () {
@@ -46,119 +45,57 @@ document
     const adminEmail = document.getElementById("adminEmail").value;
     const password = document.getElementById("password").value;
     const confirmPassword = document.getElementById("confirmPassword").value;
-    const subscriptionType =
-      document.getElementById("subscriptionSelect").value;
+    let subscriptionType = document.getElementById("subscriptionSelect").value;
 
-    if (subscriptionType === "1D") {
-      subscriptionType = "price_1PFNCKG4W5PBvrHXupfyZJJD";
-    } else if (subscriptionType === "1M") {
-      subscriptionType = "price_1PFN9VG4W5PBvrHXAtlcriX6";
-    } else if (subscriptionType === "6M") {
-      subscriptionType = "price_1PFNAcG4W5PBvrHX0I2NJKeR";
-    } else if (subscriptionType === "12M") {
-      subscriptionType = "price_1PFNB9G4W5PBvrHXPuFK2dXO";
-    }
+    // Match subscription type with Stripe price IDs
+    const subscriptionTypes = {
+      "1D": "price_1PFNCKG4W5PBvrHXupfyZJJD",
+      "1M": "price_1PFN9VG4W5PBvrHXAtlcriX6",
+      "6M": "price_1PFNAcG4W5PBvrHX0I2NJKeR",
+      "12M": "price_1PFNB9G4W5PBvrHXPuFK2dXO",
+    };
+    subscriptionType = subscriptionTypes[subscriptionType];
 
     if (password !== confirmPassword) {
       alert("Las contraseñas no coinciden.");
       return;
     }
 
+    let userCredential;
     try {
-      const userCredential = await createUserWithEmailAndPassword(
-        auth,
-        adminEmail,
-        password
-      );
-      // Registrar en Firestore la información del tenant una vez completado el pago
-      await setDoc(
-        doc(
-          db,
-          `schools - ${schoolName.replace(/\s+/g, "").toLowerCase()}`,
-          userCredential.user.uid
-        ),
-        {
-          tenant: schoolName,
-          adminEmail: adminEmail,
-          subscriptionType: subscriptionType,
-        }
-      );
+      userCredential = await createUserWithEmailAndPassword(auth, adminEmail, password);
 
-      const docRef = await db
-        .collection("customers")
-        .doc(currentUser.uid)
-        .collection("checkout_sessions")
-        .add({
-          price: subscriptionType,
-          success_url: window.location.origin,
-          cancel_url: window.location.origin,
-        });
-      // Wait for the CheckoutSession to get attached by the extension
-      docRef.onSnapshot((snap) => {
+      await setDoc(doc(db, `schools/${schoolName.replace(/\s+/g, "").toLowerCase()}/admins`, userCredential.user.uid), {
+        tenant: schoolName,
+        adminEmail: adminEmail,
+        password: password,
+        priceId: subscriptionType,
+      });
+
+      const checkoutSessionRef = await addDoc(collection(db, "customers", userCredential.user.uid, "checkout_sessions"), {
+        price: subscriptionType,
+        success_url: window.location.href = "inicioDeSesion.html",
+        cancel_url: window.location.origin,
+      });
+
+      onSnapshot(checkoutSessionRef, (snap) => {
         const { error, url } = snap.data();
         if (error) {
-          // Show an error to your customer and
-          // inspect your Cloud Function logs in the Firebase console.
-          alert(`An error occured: ${error.message}`);
+          alert(`An error occurred: ${error.message}`);
         }
         if (url) {
-          // We have a Stripe Checkout URL, let's redirect.
           window.location.assign(url);
         }
       });
-
-      // Redirigir al Checkout de Stripe
-      const checkoutSession = await payments.createCheckoutSession({
-        priceId: subscriptionType,
-        successUrl: (window.location.href = "inicioDeSesion.html"),
-        cancelUrl: window.location.href,
-      });
-
-      const stripe = await loadStripe(
-        "pk_test_51PFMprG4W5PBvrHX61Mx5dpflJ0rx00bQ3Wvhr0cLBkoXmCzgqFgzTefHmTNk6McxnijpvRvCmyWtT1iIpK7slPX00jtOsIPXP"
-      );
-      stripe.redirectToCheckout({ sessionId: checkoutSession.id });
 
       console.log("Escuela registrada con éxito en Firestore");
       alert("Procesamiento y registro de la escuela completado con éxito.");
     } catch (error) {
       console.error("Error en el proceso de registro y pago:", error);
-      await deleteUser(userCredential.user);
-      await deleteDoc(
-        doc(
-          db,
-          `schools - ${schoolName.replace(/\s+/g, "").toLowerCase()}`,
-          userDoc.id
-        )
-      );
-      alert(
-        "Error al procesar el pago y registrar la escuela. Por favor, intente nuevamente."
-      );
+      if (userCredential) {
+        await deleteUser(userCredential.user);
+        await deleteDoc(doc(db, `schools/${schoolName.replace(/\s+/g, "").toLowerCase()}/admins`, userCredential.user.uid));
+      }
+      alert("Error al procesar el pago y registrar la escuela. Por favor, intente nuevamente.");
     }
   });
-
-window.onload = async () => {
-  const querySnapshot = await getDocs(collection(db, "products"));
-  const productContainer = document.getElementById("product-container");
-  querySnapshot.forEach((doc) => {
-    const data = doc.data();
-    const productDiv = document.createElement("div");
-    productDiv.className = "product";
-    productDiv.innerHTML = `
-            <h3>${data.name}</h3>
-            <p>${data.description}</p>
-            <p>${data.price / 100} ${data.currency.toUpperCase()}</p>
-            <button onclick="startCheckout('${doc.id}')">Comprar</button>
-        `;
-    productContainer.appendChild(productDiv);
-  });
-};
-
-window.startCheckout = async (priceId) => {
-  const createCheckoutSession = httpsCallable(
-    functions,
-    "ext-firestore-stripe-payments-createCheckoutSession"
-  );
-  const { data } = await createCheckoutSession({ priceId });
-  window.location.href = data.url;
-};
